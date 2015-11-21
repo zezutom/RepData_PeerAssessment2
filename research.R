45a994cbc19e6b734ecfa1799f48d3075e69a58c
tomaszezula <tomas.zezula@panaseer.com>
2015-11-21 17:04:09 +0100

    Plotting, wip

--- research.R
+++ research.R
@@ -48,13 +48,13 @@ storm_data$YEAR <- as.numeric(
 # Reduce columns to only those needed for answering the questions
 health_data <- c("FATALITIES", "INJURIES")          # Health
 economic_data <- c("PROPDMG", "CROPDMG")            # Economy
-
+economic_data_mult <- c("PROPDMGEXP", "CROPDMGEXP")
 storm_data <- subset(storm_data, select=c(
   "YEAR",                         # Timeline
   "EVTYPE",                       # Weather Events 
   health_data,
   economic_data,
-  "PROPDMGEXP", "CROPDMGEXP"      # Economy (extent)
+  economic_data_mult      # Economy (extent)
 ))
 
 ## PROPDMGEXP and CROPDMGEXP represent the scale of property and crop damage.
@@ -86,35 +86,52 @@ hist(storm_data$YEAR,
 dev.off()
 
 # Most extreme weather events happened between 1990 - 2010
-storm_data <- subset(storm_data, (YEAR >= 1990) & (YEAR <= 2010))
+year_from <- 1990
+year_to <- 2010
+storm_data <- subset(storm_data, (YEAR >= year_from) & (YEAR <= year_to))
 
-# Summarize weather events by the given health or economic aspect. 
+# Draws a bar chart.
+# df      .. data frame containing the plotted data
+# weight  .. columnar weight
+# title   .. a distinct part of a chart title
+bar_chart <- function(df, weight, title) {
+  png(paste(title, "png", sep="."))
+  chart <- qplot(EVTYPE, data = fatalities, weight = weight, geom = "bar", binwidth = 1) + 
+    scale_y_continuous(paste("Number of", title)) + 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+    xlab("Extreme Weather Event") + 
+    ggtitle(paste(title, 
+                  "by Extreme Weather Events in the US\nbetween", 
+                  year_from, "and", year_to))  
+  print(chart)
+  dev.off()
+}
+
+# Summarizes weather events by the given health or economic aspect. 
 # Results are plotted as a histogram.
-sum_by_aspect <- function(x, debug = FALSE) {
+sum_by_aspect <- function(x, cap = 10, debug = FALSE) {
   # Summarize weather events by the given health aspect
   summary <- aggregate(subset(storm_data, select = x), list(EVTYPE = storm_data$EVTYPE), "sum")
   
   # Sort in a descending order (most frequent first)
   setorderv(summary, x, -1)
   
+  # Consider Top N (Top 10 by default) most severe events
+  top_n <- head(summary, cap)
+  
   if (debug) {
-    print(head(summary))  
+    print(top_n)  
   }
   
-  return (summary)
+  # Plot it on a bar chart
+  bar_chart(top_n, top_n[, x], x)
 }
 
 ## Q1. Across the United States, which types of events 
 ## (as indicated in the EVTYPE variable) are most harmful 
 ## with respect to population health?
-fatalities <- sum_by_aspect("FATALITIES")
-injuries <- sum_by_aspect("INJURIES")
-
-# TODO plot fatalities and injuries
+lapply(health_data, sum_by_aspect)
 
 ## Q2. Across the United States, which types of events have 
 ## the greatest economic consequences?
-property_damage <- sum_by_aspect("PROPDMG")
-crop_damage <- sum_by_aspect("CROPDMG")
-
-# TODO plot property and crop damage
+lapply(economic_data, sum_by_aspect)
